1ffed37ae8d7255129a8a6d55f0ff1486ab2e4b9
RomjanHossain <romjanhossain726526@gmail.com>
2020-02-18 15:25:01 +0600

    database

--- Ecommerce/settings/production.py
+++ Ecommerce/settings/production.py
@@ -102,7 +102,7 @@ WSGI_APPLICATION = 'Ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
